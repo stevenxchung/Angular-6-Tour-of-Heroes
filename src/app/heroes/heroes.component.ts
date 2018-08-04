7b61e95a2e54d0fd1ef44a0e1c559737be8f18e9
Steven <stevenxchung@gmail.com>
2018-08-03 20:29:32 -0400

    Step 3 - Add mock heroes list to heros component as well as onSelect click event

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // Mock Heroes
+  heroes = HEROES;
 
-  constructor() { }
+  selectedHero: Hero;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
